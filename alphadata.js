7b638b58a2a3b678bbf3baf6beb956de4504a316
ReedKrawiec <reedkrawiec@gmail.com>
2016-12-15 19:24:34 -0500

    Updated for es6 target

--- alphadata.js
+++ alphadata.js
@@ -1,18 +1,18 @@
 "use strict";
 /// <reference path="../typings/index.d.ts" />
-var _ = require("lodash");
-var fs = require("fs");
-var path = require("path");
-var mkdirp = require("mkdirp");
-var alphaDataBase = (function () {
-    function alphaDataBase(location) {
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const mkdirp = require("mkdirp");
+class alphaDataBase {
+    constructor(location) {
         this.internal_location = location;
         this.internal_schemes = [];
         this.loadDB();
     }
-    alphaDataBase.prototype.loadDB = function () {
-        var this_reference = this;
-        var dir = path.dirname(this_reference.internal_location);
+    loadDB() {
+        let this_reference = this;
+        let dir = path.dirname(this_reference.internal_location);
         mkdirp.sync(dir);
         if (fs.existsSync(this_reference.internal_location))
             this.internal_obj = JSON.parse(fs.readFileSync(this_reference.internal_location, "utf-8"));
@@ -23,26 +23,25 @@ var alphaDataBase = (function () {
         }
         else
             this.internal_schemes = {};
-    };
-    alphaDataBase.prototype.makeTable = function (name) {
+    }
+    makeTable(name) {
         if (!this.tableExists(name))
             this.internal_obj[name] = [];
         else {
             throw Error("Table already exists.");
         }
         return this;
-    };
-    alphaDataBase.prototype.deleteTable = function (name) {
+    }
+    deleteTable(name) {
         if (!this.tableExists(name))
             throw Error("Table does not exist");
         delete this.internal_obj[name];
         return this;
-    };
-    alphaDataBase.prototype.initTables = function (tables) {
-        var _this = this;
-        var this_ = this;
-        tables.forEach(function (tableData) {
-            var table;
+    }
+    initTables(tables) {
+        let this_ = this;
+        tables.forEach((tableData) => {
+            let table;
             if (typeof tableData === "object") {
                 table = tableData.name;
                 if (tableData.scheme !== undefined) {
@@ -55,41 +54,41 @@ var alphaDataBase = (function () {
             else {
                 throw new Error("Unrecognized table init format.");
             }
-            if (!_this.tableExists(table)) {
-                _this.makeTable(table);
+            if (!this.tableExists(table)) {
+                this.makeTable(table);
             }
         });
         return this;
-    };
-    alphaDataBase.prototype.tableExists = function (name) {
-        var this_reference = this;
+    }
+    tableExists(name) {
+        let this_reference = this;
         return (Object.keys(this_reference.internal_obj).indexOf(name) > -1);
-    };
-    alphaDataBase.prototype.select = function (input) {
-        var this_reference = this;
+    }
+    select(input) {
+        let this_reference = this;
         if (typeof input === "string") {
-            var temp_obj = {};
+            let temp_obj = {};
             temp_obj[input] = _.range(0, this_reference.internal_obj[input].length);
             this.internal_selected_items = temp_obj;
         }
         else if (typeof input === "function") {
-            var keys = Object.keys(this_reference.internal_obj);
-            var passing_tables = keys.filter(input);
-            var temp_obj_1 = {};
-            passing_tables.forEach(function (table_name) {
-                temp_obj_1[table_name] = _.range(0, this_reference.internal_obj[table_name].length);
-                this_reference.internal_selected_items = temp_obj_1;
+            let keys = Object.keys(this_reference.internal_obj);
+            let passing_tables = keys.filter(input);
+            let temp_obj = {};
+            passing_tables.forEach((table_name) => {
+                temp_obj[table_name] = _.range(0, this_reference.internal_obj[table_name].length);
+                this_reference.internal_selected_items = temp_obj;
             });
         }
         return this;
-    };
-    alphaDataBase.prototype.where = function (input) {
-        var this_reference = this;
-        var keys = Object.keys(this_reference.internal_selected_items);
-        keys.forEach(function (key) {
-            var passing_items = [];
-            this_reference.internal_selected_items[key].forEach(function (index) {
-                var condition = input(this_reference.internal_obj[key][index]);
+    }
+    where(input) {
+        let this_reference = this;
+        let keys = Object.keys(this_reference.internal_selected_items);
+        keys.forEach((key) => {
+            let passing_items = [];
+            this_reference.internal_selected_items[key].forEach((index) => {
+                let condition = input(this_reference.internal_obj[key][index]);
                 if (condition) {
                     passing_items.push(index);
                 }
@@ -97,42 +96,42 @@ var alphaDataBase = (function () {
             this_reference.internal_selected_items[key] = passing_items;
         });
         return this;
-    };
-    alphaDataBase.prototype.getSelected = function (fields) {
-        var this_reference = this;
-        var keys = Object.keys(this_reference.internal_selected_items);
-        var obj_to_return = [];
-        keys.forEach(function (key) {
-            this_reference.internal_selected_items[key].forEach(function (val) {
+    }
+    getSelected(fields) {
+        let this_reference = this;
+        let keys = Object.keys(this_reference.internal_selected_items);
+        let obj_to_return = [];
+        keys.forEach((key) => {
+            this_reference.internal_selected_items[key].forEach((val) => {
                 if (fields !== undefined && fields.length > 0 && Array.isArray(fields)) {
-                    var obj_with_selected_1 = {};
-                    fields.forEach(function (field) {
-                        obj_with_selected_1[field] = this_reference.internal_obj[key][val][field];
+                    let obj_with_selected = {};
+                    fields.forEach((field) => {
+                        obj_with_selected[field] = this_reference.internal_obj[key][val][field];
                     });
-                    obj_to_return.push(obj_with_selected_1);
+                    obj_to_return.push(obj_with_selected);
                 }
                 else
                     obj_to_return.push(this_reference.internal_obj[key][val]);
             });
         });
         return obj_to_return;
-    };
-    alphaDataBase.prototype.insert = function (input) {
-        var this_reference = this;
-        var keys = Object.keys(this_reference.internal_selected_items);
-        keys.forEach(function (key) {
-            var valid = true;
+    }
+    insert(input) {
+        let this_reference = this;
+        let keys = Object.keys(this_reference.internal_selected_items);
+        keys.forEach((key) => {
+            let valid = true;
             if (this_reference.internal_schemes[key] !== undefined) {
-                var scheme_keys = Object.keys(this_reference.internal_schemes[key]);
-                scheme_keys.forEach(function (scheme_key) {
+                let scheme_keys = Object.keys(this_reference.internal_schemes[key]);
+                scheme_keys.forEach((scheme_key) => {
                     if (typeof this_reference.internal_schemes[key][scheme_key] === "string") {
                         if (typeof input[scheme_key] !== this_reference.internal_schemes[key][scheme_key]) {
-                            throw new Error("While inserting to " + key + ", type of " + scheme_key + " (" + typeof input[scheme_key] + ") did not match the scheme type, which was " + this_reference.internal_schemes[key][scheme_key]);
+                            throw new Error(`While inserting to ${key}, type of ${scheme_key} (${typeof input[scheme_key]}) did not match the scheme type, which was ${this_reference.internal_schemes[key][scheme_key]}`);
                         }
                     }
                     else if (typeof this_reference.internal_schemes[key][scheme_key] === "function") {
                         if (!this_reference.internal_schemes[key][scheme_key](input[scheme_key])) {
-                            throw new Error("While inserting to " + key + ", Function for " + scheme_key + " did not return true.");
+                            throw new Error(`While inserting to ${key}, Function for ${scheme_key} did not return true.`);
                         }
                     }
                 });
@@ -142,34 +141,33 @@ var alphaDataBase = (function () {
             }
         });
         return this;
-    };
-    alphaDataBase.prototype.edit = function (func) {
-        var this_reference = this;
-        var keys = Object.keys(this_reference.internal_selected_items);
-        keys.forEach(function (key) {
+    }
+    edit(func) {
+        let this_reference = this;
+        let keys = Object.keys(this_reference.internal_selected_items);
+        keys.forEach((key) => {
             this_reference.internal_obj[key].forEach(func);
         });
         return this;
-    };
-    alphaDataBase.prototype.write = function () {
-        var this_reference = this;
-        var dir = path.dirname(this_reference.internal_location);
+    }
+    write() {
+        let this_reference = this;
+        let dir = path.dirname(this_reference.internal_location);
         fs.writeFileSync(this_reference.internal_location, JSON.stringify(this_reference.internal_obj));
         fs.writeFileSync(path.join(dir, "schemes.sch"), JSON.stringify(this_reference.internal_schemes));
-    };
-    alphaDataBase.prototype.deleteItem = function () {
-        var this_reference = this;
-        var keys = Object.keys(this_reference.internal_selected_items);
-        keys.forEach(function (key) {
-            for (var a = this_reference.internal_obj[key].length; a > 0; a--) {
+    }
+    deleteItem() {
+        let this_reference = this;
+        let keys = Object.keys(this_reference.internal_selected_items);
+        keys.forEach((key) => {
+            for (let a = this_reference.internal_obj[key].length; a > 0; a--) {
                 if (this_reference.internal_selected_items[key].indexOf(a - 1) > -1) {
                     this_reference.internal_obj[key].splice(a - 1, 1);
                 }
             }
         });
         return this;
-    };
-    return alphaDataBase;
-}());
+    }
+}
 module.exports = alphaDataBase;
 //# sourceMappingURL=alphadata.js.map
